c0d1e8431670e03c85ba412e1c5c372ef32fef9c
Gilberto Lopes <gclopes97@hotmail.com>
2020-08-20 09:57:47 -0300

    Configuracao do provider msl e primeira migracao

---
+++ WebAPI/Data/DonationDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+
+namespace WebAPI.Data
+{
+    public class DonationDBContext : DbContext
+    {
+        public DonationDBContext(DbContextOptions<DonationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DCandidate> dCandidates { get; set; }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebAPI.Data;
 
 namespace WebAPI
 {
@@ -25,6 +27,12 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //DbContext injection
+            services.AddDbContext<DonationDBContext>(options =>
+            //Adicionar o provider do mysql usando = Install-Package Pomelo.EntityFrameworkCore.MySql
+                    options.UseMySql(Configuration.GetConnectionString("DonationDBContext"),
+                        builder => builder.MigrationsAssembly("WebAPI")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
